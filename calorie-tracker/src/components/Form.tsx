b99a353e1bab2e78ed0bc7d01ec22316a61f56aa
joellarramendia <joellarramendia@gmail.com>
2025-12-03 16:54:31 -0300

    Add logic and validation to the form

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,6 +1,29 @@
+import {useState} from "react"
 import { categories } from "../data/categories"
+import type { Activity } from "../types"
 
 export default function Form() {
+
+    const [activity, setActivity] = useState<Activity>({
+        category: 1,
+        name: '',
+        calories: 0
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ['category', 'calories'].includes(e.target.id)
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
+        })
+    }
+
+    const isValidActivity = () => {
+        const {name, calories} = activity
+        console.log(name.trim() !== '')
+        return name.trim() !== '' && calories > 0
+    }
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
         <div className="grid grid-cols-1">
@@ -8,6 +31,8 @@ export default function Form() {
             <select
                 className="border border-slate-300 p-2 rounded-lg w-full bg-white" 
                 id="category"
+                value={activity.category}
+                onChange={handleChange}
             >
                 {categories.map(category => (
                     <option 
@@ -21,12 +46,14 @@ export default function Form() {
         </div>
 
         <div className="grid grid-cols-1">
-            <label htmlFor="activity" className="font-bold">Actividad:</label>
+            <label htmlFor="name" className="font-bold">Actividad:</label>
             <input 
-                id="activity" 
+                id="name" 
                 type="text"
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+                value={activity.name}
+                onChange={handleChange}
             />
         </div>
 
@@ -37,13 +64,16 @@ export default function Form() {
                 type="number"
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Calorias. Ej. 300 o 500"
+                value={activity.calories}
+                onChange={handleChange}
             />
         </div>
 
         <input 
             type="submnit"
-            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white"
-            value='Guardar Comida o Ejercicio'
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white disabled:opacity-10"
+            value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'}
+            disabled={!isValidActivity()}
         />
     </form>
   )
