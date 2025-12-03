30ee789ad3425c517d09a075a26372649a7450cb
joellarramendia <joellarramendia@gmail.com>
2025-12-03 16:05:08 -0300

    first commit

---
+++ calorie-tracker/src/components/Form.tsx
@@ -0,0 +1,50 @@
+import { categories } from "../data/categories"
+
+export default function Form() {
+  return (
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <div className="grid grid-cols-1">
+            <label htmlFor="category" className="font-bold">Categoria:</label>
+            <select
+                className="border border-slate-300 p-2 rounded-lg w-full bg-white" 
+                id="category"
+            >
+                {categories.map(category => (
+                    <option 
+                        key={category.id}
+                        value={category.id}
+                    >
+                        {category.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+
+        <div className="grid grid-cols-1">
+            <label htmlFor="activity" className="font-bold">Actividad:</label>
+            <input 
+                id="activity" 
+                type="text"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+            />
+        </div>
+
+        <div className="grid grid-cols-1">
+            <label htmlFor="calories" className="font-bold">Calorias:</label>
+            <input 
+                id="calories" 
+                type="number"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Calorias. Ej. 300 o 500"
+            />
+        </div>
+
+        <input 
+            type="submnit"
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white"
+            value='Guardar Comida o Ejercicio'
+        />
+    </form>
+  )
+}
