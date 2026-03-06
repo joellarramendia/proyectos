ec0a779788268320ab977ccdaf2e0c7fe6dafcf6
joellarramendia <joellarramendia@gmail.com>
2026-03-05 23:36:55 -0300

    refactor: implemented useReducer

---
+++ calculadora_propinas-useReducer/src/components/OrderContents.tsx
@@ -0,0 +1,42 @@
+import { formatCurrency } from "../helpers";
+import type { OrderActions } from "../reducers/order-reducer";
+import type { OrderItem } from "../types";
+
+type orderContentsProps = {
+    order: OrderItem[],
+    dispatch: React.ActionDispatch<[action: OrderActions]>
+}
+
+export default function OrderContents({ order, dispatch }: orderContentsProps) {
+    return (
+        <div>
+            <h2 className='font-black text-4xl'>Consumo</h2>
+
+            <div className="space-y-3 mt-10">
+                {order.map(item => (
+                            <div
+                                className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b"
+                                key={item.id}>
+
+                                <div>
+                                    <p className="text-lg">
+                                        {item.name} - {formatCurrency(item.price)}
+                                    </p>
+                                    <p className="font-black">
+                                        Cantidad: {item.quantity} - {formatCurrency(item.price * item.quantity)}
+                                    </p>
+
+                                </div>
+
+                                <button
+                                    className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
+                                    onClick={() => dispatch({ type: 'remove-item', payload: { id: item.id } })}>
+                                    X
+                                </button>
+                            </div>
+                        ))
+                    }
+            </div>
+        </div>
+    )
+}
---
+++ calculadora_propinas-useReducer/src/components/TipPercentage.tsx
@@ -0,0 +1,49 @@
+import type { OrderActions } from "../reducers/order-reducer"
+
+const tipOptions = [
+    {
+        id: 'tip-10',
+        value: .10,
+        label: '10%'
+    },
+    {
+        id: 'tip-20',
+        value: .20,
+        label: '20%'
+    },
+    {
+        id: 'tip-50',
+        value: .50,
+        label: '50%'
+    },
+]
+
+type TipPercentageProps = {
+    dispatch: React.ActionDispatch<[action: OrderActions]>,
+    tip: number
+}
+
+export default function TipPercentage({ dispatch, tip }: TipPercentageProps) {
+    return (
+        <div>
+            <h3 className="font-black text-2xl">Propina:</h3>
+
+            <form>
+                {tipOptions.map(tipOptions => (
+                    <div key={tipOptions.id} className="flex gap-2">
+                        <label htmlFor={tipOptions.id}>{tipOptions.label}</label>
+                        <input
+                            id={tipOptions.id}
+                            type="radio"
+                            name="tip"
+                            value={tipOptions.value}
+                            onChange={ e => dispatch({type: 'add-tip', payload: {value: +e.target.value}})}
+                            checked={tip === tipOptions.value}
+                        />
+                    </div>
+                ))}
+
+            </form>
+        </div>
+    )
+}
