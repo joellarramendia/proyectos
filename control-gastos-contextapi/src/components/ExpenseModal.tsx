8150874c3c9c3fe74b0113c0fd8265406ab8abe4
joellarramendia <joellarramendia@gmail.com>
2026-03-09 23:28:45 -0300

    feat: add ExpenseForm component and integrate date pickers for expense tracking

--- control-gastos-contextapi/src/components/ExpenseModal.tsx
+++ control-gastos-contextapi/src/components/ExpenseModal.tsx
@@ -1,22 +1,27 @@
 import { Fragment } from 'react'
 import { PlusCircleIcon } from '@heroicons/react/24/solid'
-import { Dialog, Transition } from '@headlessui/react'
+import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react'
+import { useBudget } from '../hooks/useBudget'
+import  ExpenseForm from './ExpenseForm'
 
 export default function ExpenseModal() {
+
+    const { state, dispatch } = useBudget()
 
   return (
     <>
       <div className="fixed right-5 bottom-5 flex items-center justify-center">
         <button
           type="button"
+          onClick={() => dispatch({type: 'show-modal'})}
         >
           <PlusCircleIcon className='w-16 h-16 text-blue-600 rounded-full' />
         </button>
       </div>
 
-      <Transition appear show={false} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={() => {}}>
-          <Transition.Child
+      <Transition appear show={state.modal} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={() => dispatch({type: 'hide-modal'})}>
+          <TransitionChild
             as={Fragment}
             enter="ease-out duration-300"
             enterFrom="opacity-0"
@@ -26,11 +31,11 @@ export default function ExpenseModal() {
             leaveTo="opacity-0"
           >
             <div className="fixed inset-0 bg-black bg-opacity-75" />
-          </Transition.Child>
+          </TransitionChild>
 
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
+              <TransitionChild
                 as={Fragment}
                 enter="ease-out duration-300"
                 enterFrom="opacity-0 scale-95"
@@ -39,12 +44,12 @@ export default function ExpenseModal() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <DialogPanel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
     
-
+                    <ExpenseForm />
     
-                </Dialog.Panel>
-              </Transition.Child>
+                </DialogPanel>
+              </TransitionChild>
             </div>
           </div>
         </Dialog>
--- control-gastos-contextapi/src/reducers/budget-reducer.ts
+++ control-gastos-contextapi/src/reducers/budget-reducer.ts
@@ -1,13 +1,17 @@
 
 export type BudgetActions =
-    { type: 'add-budget', payload: { budget: number } }
+    { type: 'add-budget', payload: { budget: number } } |
+    { type: 'show-modal' } |
+    { type: 'hide-modal' }
 
 export type BudgetState = {
-    budget: number
+    budget: number,
+    modal: boolean
 }
 
 export const initialState: BudgetState = {
-    budget: 0
+    budget: 0,
+    modal: false
 }
 
 export const budgetReducer = (
@@ -15,11 +19,26 @@ export const budgetReducer = (
     action: BudgetActions
 ) => {
 
-    if(action.type === 'add-budget') {
-        return{
+    if (action.type === 'add-budget') {
+        return {
             ...state,
             budget: action.payload.budget
         }
     }
+
+    if (action.type === 'show-modal') {
+        return {
+            ...state,
+            modal: true
+        }
+    }
+
+    if (action.type === 'hide-modal') {
+        return {
+            ...state,
+            modal: false
+        }
+    }
+
     return state
 }
