4e3f68392a9890864c46d488f41f612cac27a63c
joellarramendia <joellarramendia@gmail.com>
2026-03-10 21:04:26 -0300

    feat: integrate swipeable list for expense actions, add editing functionality in ExpenseForm, and enhance budget reducer for expense management

--- control-gastos-contextapi/src/components/ExpenseForm.tsx
+++ control-gastos-contextapi/src/components/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { DraftExpense, Value } from "../types";
 import { categories } from "../data/categories";
 import DatePicker from 'react-date-picker';
@@ -18,7 +18,14 @@ export default function ExpenseForm() {
     })
 
     const [error, setError] = useState('')
-    const { dispatch } = useBudget()
+    const { dispatch, state } = useBudget()
+
+    useEffect(() => {
+        if(state.editingId) {
+            const editingExpense = state.expenses.filter(currentExpense => currentExpense.id === state.editingId)[0]
+            setExpense(editingExpense)
+        }``
+    }, [state.editingId])
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement> | React.ChangeEvent<HTMLSelectElement, HTMLSelectElement>) => {
         const {name, value} = e.target
--- control-gastos-contextapi/src/reducers/budget-reducer.ts
+++ control-gastos-contextapi/src/reducers/budget-reducer.ts
@@ -1,28 +1,32 @@
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 import type { DraftExpense, Expense } from "../types"
 
 export type BudgetActions =
     { type: 'add-budget', payload: { budget: number } } |
     { type: 'show-modal' } |
     { type: 'hide-modal' } |
-    { type: 'add-expense', payload: { expense: DraftExpense } } 
+    { type: 'add-expense', payload: { expense: DraftExpense } } |
+    { type: 'delete-expense', payload: { id: Expense['id'] } } |
+    { type: 'get-expense-by-id', payload: { id: Expense['id'] } }
 
 export type BudgetState = {
     budget: number,
     modal: boolean,
     expenses: Expense[]
+    editingId: Expense['id']
 }
 
 export const initialState: BudgetState = {
     budget: 0,
     modal: false,
-    expenses: []
+    expenses: [],
+    editingId: ''
 }
 
-const createExpense = (draftExpense: DraftExpense) : Expense => {
+const createExpense = (draftExpense: DraftExpense): Expense => {
     return {
         ...draftExpense,
-        id: uuidv4() 
+        id: uuidv4()
     }
 }
 
@@ -58,9 +62,26 @@ export const budgetReducer = (
             ...state,
             expenses: [...state.expenses, expense],
             modal: false
-            
+
         }
     }
+
+    if (action.type === 'delete-expense') {
+        return {
+            ...state,
+            expenses: state.expenses.filter(expense => expense.id !== action.payload.id)
+
+        }
+    }
+
+    if (action.type === 'get-expense-by-id') {
+        return{
+            ...state,
+            editingId: action.payload.id,
+            modal: true
+        }
+    }
+
 
     return state
 }
