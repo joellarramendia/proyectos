e27a73f016dabc37aaa9b2fea55af8b3e758a25b
joellarramendia <joellarramendia@gmail.com>
2026-03-12 16:11:52 -0300

    feat: enhance UI with styles and form functionality for weather search

--- clima-react-typescript/src/App.tsx
+++ clima-react-typescript/src/App.tsx
@@ -1,10 +1,16 @@
-
+import styles from './App.module.css';
+import Form from './components/Form/Form';
 
 function App() {
 
   return (
     <>
-      <h1>Hola</h1>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+
+      <div className={styles.container}>
+        <p><Form/></p>
+        <p>2</p>
+      </div>
     </>
   )
 }
