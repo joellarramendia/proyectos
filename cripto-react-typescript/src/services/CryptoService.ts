11ff37f6259341ea1c82b3487eb0f8610c21c3fe
joellarramendia <joellarramendia@gmail.com>
2026-03-16 21:02:53 -0300

    feat: add cryptocurrency fetching functionality and update schemas

---
+++ cripto-react-typescript/src/services/CryptoService.ts
@@ -0,0 +1,13 @@
+import axios from "axios"
+import { CryptoCurrenciesResponseSchema } from "../schema/crypto-schema"
+
+
+export async function getCryptos () {
+    //const url = 'https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,SUPPLY,PRICE,MKT_CAP,VOLUME,CHANGE,TOPLIST_RANK&toplist_quote_asset=USD'
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+    const {data: {Data}} = await axios(url)
+    const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+    if(result.success){
+        return result.data
+    }
+}
---
+++ cripto-react-typescript/src/store.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand"
+import {devtools} from 'zustand/middleware'
+import type { Cryptocurrency } from "./types"
+import { getCryptos } from "./services/CryptoService"
+
+type CryptoStore = {
+   cryptocurrencies: Cryptocurrency[],
+   fetchCryptos: () => Promise<void>
+}
+
+
+export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
+    cryptocurrencies: [],
+    fetchCryptos: async () => {
+        const cryptocurrencies = await getCryptos()
+        set(() => ({
+            cryptocurrencies
+        }))
+    }
+})))
