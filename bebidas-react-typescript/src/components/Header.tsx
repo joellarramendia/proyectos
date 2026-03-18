014082037af1e59c7f50f352c0695b0b2c9f0718
joellarramendia <joellarramendia@gmail.com>
2026-03-17 23:12:35 -0300

    feat: implement search functionality in Header component; update state management and services for recipe filtering

--- bebidas-react-typescript/src/components/Header.tsx
+++ bebidas-react-typescript/src/components/Header.tsx
@@ -1,9 +1,15 @@
-import { useEffect, useMemo } from "react"
+import { useEffect, useMemo, useState} from "react"
 import { NavLink, useLocation } from "react-router-dom"
 import { useAppStore } from "../stores/useAppStore"
 
 
 export default function Header() {
+
+    const [searchFilters, setSearchFilters] = useState({
+        ingredient: '',
+        category: ''
+
+    })
 
     const { pathname } = useLocation()
 
@@ -11,12 +17,32 @@ export default function Header() {
 
     const fetchCategories = useAppStore((state) => state.fetchCategories)
     const categories = useAppStore((state) => state.categories)
+    const searchRecipes = useAppStore((state) => state.searchRecipes)
 
 
 
     useEffect(() => {
         fetchCategories()
     }, [])
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement> | React.ChangeEvent<HTMLSelectElement, HTMLSelectElement>) => {
+        setSearchFilters({
+            ...searchFilters,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        //TODO: Validar
+        if(Object.values(searchFilters).includes('')){
+            console.log('todos los campos son')
+            return
+        }
+
+        //Consultar las recetas
+        searchRecipes(searchFilters)
+    }
 
     return (
         <header className={isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800'}>
@@ -43,7 +69,10 @@ export default function Header() {
                     </nav>
                 </div>
                 {isHome && (
-                    <form className="md-w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+                    <form 
+                    className="md-w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+                    onSubmit={handleSubmit}
+                    >
                         <div className="space-y-4 ">
                             <label
                                 htmlFor="ingredient"
@@ -58,22 +87,26 @@ export default function Header() {
                                 name="ingredient"
                                 className="p-3 w-full rounded-lg focus:outline-none bg-white"
                                 placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Cafe"
+                                onChange={handleChange}
+                                value={searchFilters.ingredient}
 
                             />
                         </div>
 
                         <div className="space-y-4 ">
                             <label
-                                htmlFor="ingredient"
+                                htmlFor="category"
                                 className="block text-white uppercase font-extrabold text-lg"
                             >
                                 Categoria
                             </label>
 
                             <select
-                                id="ingredient"
-                                name="ingredient"
+                                id="category"
+                                name="category"
                                 className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                                onChange={handleChange}
+                                value={searchFilters.category}
                             >
                                 <option value="">-- Seleccione --</option>
                                 {categories.drinks.map(category => (
