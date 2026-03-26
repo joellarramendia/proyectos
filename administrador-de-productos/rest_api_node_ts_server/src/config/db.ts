bc841b79711f8fa0342e5cabb0418ed7aaef7fe2
joellarramendia <joellarramendia@gmail.com>
2026-03-25 22:09:58 -0300

    fix: update Sequelize config with SSL and dynamic model paths

--- administrador-de-productos/rest_api_node_ts_server/src/config/db.ts
+++ administrador-de-productos/rest_api_node_ts_server/src/config/db.ts
@@ -1,22 +1,23 @@
 import { Sequelize } from "sequelize-typescript";
-import {fileURLToPath} from 'url'
-import { dirname } from "path";
 import dotenv from 'dotenv'
-import Product from "../models/Product.model";
 
-// Recreamos __dirname
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
 
 dotenv.config()
 
+
 // const db = new Sequelize(process.env.DATABASE_URL, {
-//     models: [__dirname + '/../models/**/*.ts']
+//     models: [Product],
+//     logging: false
 // })
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-    models: [Product],
-    logging: false
+const db = new Sequelize(process.env.DATABASE_URL!, {
+    models: [__dirname + '/../models/**/*.ts'],
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: 'true'
+        }
+    }
 })
 
 export default db
